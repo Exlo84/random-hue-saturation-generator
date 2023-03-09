85b6ede676c5fdf710340119ea29eb1b9a67eb86
Exlo <46317321+Exlo84@users.noreply.github.com>
2023-03-09 23:35:42 +0100

    Changed to sharp and added json to the output
    This code change uses sharp to read the input image as a stream, and applies the color adjustments to each copy in parallel to speed up the process.

--- script.js
+++ script.js
@@ -1,31 +1,52 @@
-const Jimp = require('jimp');
-const fs = require('fs');
+const sharp = require('sharp');
+const fs = require('fs').promises;
 
 // path to the original image file
 const originalFilePath = './master-image.png';
 
 // create output directory if it does not exist
 const outputDir = './output';
-if (!fs.existsSync(outputDir)) {
-  fs.mkdirSync(outputDir);
-}
+fs.mkdir(outputDir, { recursive: true });
+
+// create JSON directory within output directory if it does not exist
+const jsonDir = `${outputDir}/JSON`;
+fs.mkdir(jsonDir, { recursive: true });
 
 // number of variations to generate
 const numVariations = 10000;
 
-// loop through and generate variations
-for (let i = 1; i <= numVariations; i++) {
-  // load original image
-  Jimp.read(originalFilePath, (err, image) => {
-    if (err) throw err;
+(async () => {
+  try {
+    // load original image
+    const image = await sharp(originalFilePath);
 
-    // randomly adjust hue and saturation
-    const hue = Math.floor(Math.random() * 360);
-    const saturation = Math.floor(Math.random() * 100);
-    image.color([{ apply: 'hue', params: [hue] }, { apply: 'saturate', params: [saturation] }]);
+    // create copies of original image
+    const images = Array(numVariations).fill().map(() => image.clone());
 
-    // save variation to output directory
-    const outputFilePath = `${outputDir}/variation_${i}.png`;
-    image.write(outputFilePath);
-  });
-}
+    // apply hue and saturation adjustments to each copy in parallel
+    const colorObjs = await Promise.all(images.map(async (img, i) => {
+      const hue = Math.floor(Math.random() * 360);
+      const saturation = Math.floor(Math.random() * 100);
+      await img.modulate({ hue, saturation });
+      return {
+        hue,
+        saturation,
+        outputFilePath: `${outputDir}/variation_${i + 1}.png`,
+        jsonFilePath: `${jsonDir}/variation_${i + 1}.json`
+      };
+    }));
+
+    // save all variations and JSON files to disk at once
+    const writePromises = colorObjs.map(({ outputFilePath, jsonFilePath }, i) => {
+      const img = images[i];
+      const writeImgPromise = img.toFile(outputFilePath);
+      const writeJsonPromise = fs.writeFile(jsonFilePath, JSON.stringify(colorObjs[i]));
+      return Promise.all([writeImgPromise, writeJsonPromise]);
+    });
+
+    await Promise.all(writePromises);
+    console.log('All variations saved successfully!');
+  } catch (err) {
+    console.error('Error generating variations:', err);
+  }
+})();
